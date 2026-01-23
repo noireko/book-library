44958da6b146379a18aa5b2a204504dac78fd74f
noireko <agustinfernandezrolon@gmail.com>
2026-01-22 23:07:32 -0300

    Modified variable names

--- js/js.js
+++ js/js.js
@@ -3,11 +3,11 @@
 // 3- Se pasa el objeto del nuevo Book a la lista de Libreria
 // 4- Hacer funcion de loop que vaya colocando los datos del array en display
 
-const title = document.getElementById("inputTitle");
-const author = document.getElementById("inputAuthor");
-const year = document.getElementById("inputYear");
-const pages = document.getElementById("inputPages");
-const read = document.getElementById("inputRead");
+const inputTitle = document.getElementById("inputTitle");
+const inputAuthor = document.getElementById("inputAuthor");
+const inputYear = document.getElementById("inputYear");
+const inputPages = document.getElementById("inputPages");
+const inputRead = document.getElementById("inputRead");
 const submitBtn = document.getElementById("agregar");
 
 const myLibrary = [];
@@ -21,11 +21,11 @@ function Book(title, author, year, pages, read) {
 }
 
 submitBtn.addEventListener("click", () => {
-    const title = title.value;
-    const author = author.value;
-    const year = year.value;
-    const pages = pages.value;
-    const read = read.value;
+    const title = inputTitle.value;
+    const author = inputAuthor.value;
+    const year = inputYear.value;
+    const pages = inputPages.value;
+    const read = inputRead.value;
     
     const newBook = new Book(title, author, year, pages, read);
 
@@ -33,9 +33,9 @@ submitBtn.addEventListener("click", () => {
 
     console.log(myLibrary);
 
-    title.value = "";
-    author.value = "";
-    year.value = "";
-    pages.value = "";
-    read.value = "";
+    inputTitle.value = "";
+    inputAuthor.value = "";
+    inputYear.value = "";
+    inputPages.value = "";
+    inputRead.value = "";
 })
